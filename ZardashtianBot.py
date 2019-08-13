e327f62e6a266fac89f4b8be9aa44c11bc280e39
GreatZardasht <36709736+GreatZardasht@users.noreply.github.com>
2019-08-13 13:35:52 +0100

    Update ZardashtianBot.py

--- ZardashtianBot.py
+++ ZardashtianBot.py
@@ -156,8 +156,9 @@ async def on_ready():
     print(client.user.name)
     print('Why Is Asyncio Bullying Us')
     print('Servers connected to:')
-    for server in client.guilds:
-      print(guilds.name)
+        for server in client.servers:
+            print(server.name)
+        await asyncio.sleep(600)
 	
 # # # Owner Only Cmds # # #
 
