afcc040c1ca2a4ae0d12fa11c98b83b4c09af082
GreatZardasht <36709736+GreatZardasht@users.noreply.github.com>
2019-09-04 08:26:50 +0100

    Update ZardashtianBot.py

--- ZardashtianBot.py
+++ ZardashtianBot.py
@@ -166,7 +166,7 @@ async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print('Why Is Asyncio Bullying Us')
-    await bot.change_presence(game=discord.Game(name='ZardashtianBot│ Z-help │ Code: Vcomeback Edition'))
+    await client.change_presence(game=discord.Game(name='ZardashtianBot│ Z-help │ Code: Vcomeback Edition'))
     	
 # # # Owner Only Cmds # # #
 
